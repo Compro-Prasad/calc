06987fed7abb2c97c2e1c524e497c46d49b4c20a
Compro-Prasad <comproprasad@gmail.com>
2017-02-18 19:59:10 +0530

    Removed build errors

--- src/calc_cmd_action.cpp
+++ src/calc_cmd_action.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <readline/history.h>
 
 #include <str.hpp>
--- src/parse_expr.cpp
+++ src/parse_expr.cpp
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <readline/history.h>
 
 #include <operators.hpp>
